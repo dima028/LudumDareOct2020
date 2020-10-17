5f9a70221b96567256dfff80c4b4a3a5b03936cb
Dima Ismail <dismail@uoguelph.ca>
2020-10-17 00:00:16 -0400

    removed unneeded print

--- src/main/java/adventure/Game.java
+++ src/main/java/adventure/Game.java
@@ -59,9 +59,7 @@ public class Game{
             // assume room doesn't exist unless proven otherwise
             boolean invalidRoom = true;
             System.out.println(currentRoomId);
-            JSONObject currentRoomBody = (JSONObject) roomObject' // .get(currentRoomId);
-            System.out.println("currentRoomId object body: ");
-            System.out.println(currentRoomBody);
+            JSONObject currentRoomBody = (JSONObject) roomObject.get(currentRoomId);
             
             // // loop through all entrances in current room
             // for(Object currentEntrance : currentRoomEntrances) {
